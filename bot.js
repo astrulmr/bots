59b1c0500e5b97acfdb84d1ff5b0319e3d710740
astrulmr <52928390+astrulmr@users.noreply.github.com>
2019-07-25 20:07:25 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -110,11 +110,11 @@ client.on('ready' , () => {
     
                              client.on('message', message => {
     if (message.content === 'how dare you') {
-       message.channel.send("do you want me to leave");
+       message.channel.send("do you want me to leave");}
 else (message.content === 'yes') {
        message.channel.send("Ok bye");
    }
-   }
+   
  })   
      
   
