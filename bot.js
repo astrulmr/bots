232f55f7d2ca79f0e2a67efc5642a49bd847b08e
astrulmr <52928390+astrulmr@users.noreply.github.com>
2019-07-27 02:26:06 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -182,7 +182,7 @@ client.on('ready' , () => {
  })
                                      client.on('message', message => {
     if (message.content === 'why') {
-       message.channel.send("I'm not your "baby"");
+       message.channel.send("I'm not your baby");
    }
  })
                                          client.on('message', message => {
@@ -212,7 +212,7 @@ client.on('ready' , () => {
    }
  })
                                                  client.on('message', message => {
-    if (message.content === 'i'm sad') {
+    if (message.content === 'im sad') {
        message.channel.send("I shouldn't have done that");
    }
  })
@@ -238,67 +238,8 @@ client.on('ready' , () => {
 
    }
  })  
- var s;
- var a;
- client.on('message', message => {
-  if (!message.guild) return
-  if (message.content.startsWith('!Rrunner')) {
-  const user = message.mentions.users.first()
-  if (user) {
-  const member = message.guild.member(user)
-  if (member) {
-s=s+1;
- s= "He Is A Runner";
-user = user.s;
-message.channel.send("All set")
- }}}
+
 })
-const pugSize = 50; // Maximum amount of players in the PUG
-const pugMembers = []; // Array to hold the members in the PUG
-
-function checkPugSize(){
-    if (pugMembers.length == 50){
-        //TODO Create the two teams
-        console.log(`PUG IS FULL: ${pugMembers.length}`);
-    }else{
-        console.log(`THE PUG IS NOT FULL: ${pugMembers.length}`);
-    }
-}
-
-function addUserPug(message){
-    // console.log(msg.author);
-    // Add user to the pugMembers Array if the array is not full
-    if (pugMembers.length <= 50){
-      
-        pugMembers.push(message.author.username);
-    }else{ // Create a new pug and pass the user into the array
-        console.log("TODO: Create a new pug when current array is filled");
-        // createNewPug(msg.author.username);
-    }
-    message.channel.send(`${message.author} added to queue ${pugMembers.length}/50.`); // Mention the user that they are added into the queue
-    // msg.reply(' added to queue. ' + `${pugMembers.length}/6`);
-    message.delete()
-    .then(message => console.log(pugMembers))
-    .catch(console.error);
-}
-
-client.on('ready', () => {
-    console.log(`Logged in as ${client.user.tag}!`);
-});
-
-client.on('message', message => {
-    if (message.content == '!size'){
-        message.channel.send(`Current PUG size: ${pugMembers.length}`);
-    }
-    if (message.content === '!add'){
-        // console.log(msg.author);
-        if(author.username) return;
-    
-        else {
-        checkPugSize();
-        addUserPug(message);}
-    }
-});
 client.on('message', message => {
   if (!message.guild) return
   if (message.content.startsWith('!power')) {
@@ -316,7 +257,6 @@ message.channel.send("power not set")
 }}
 })
 
-})
   client.on('message', message => {
     if (!message.guild) return
     if (message.content.startsWith('!who')) {
