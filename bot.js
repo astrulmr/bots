7f977a4319d831045878360083f50dc394c52cd8
astrulmr <52928390+astrulmr@users.noreply.github.com>
2019-07-27 19:25:10 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -51,9 +51,9 @@ client.on('ready' , () => {
     //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
       client.on('message', message => {
     if (message.content === 'hi') {
-       message.channel.send("hi there"`,${message.author},`);
+       message.channel.send("hi there",`${message.author},`);
        const attachment = new Attachment('./ok.jpg');
-       message.channel.send(attachment);
+       message.channel.send( attachment);
    
 
    }
