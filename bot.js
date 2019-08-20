73dac676425d2556625528bf322023d44e8c281e
astrulmr <52928390+astrulmr@users.noreply.github.com>
2019-08-20 16:43:14 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -425,7 +425,7 @@ client.on('ready' , () => {
       client.on('message', message => {
     if (message.content === 'lol') {
         const attachment = new Attachment('./lol.png');
-        bchannel.send(attachment);
+         bChannel.send(attachment);
 
    }
  })  
