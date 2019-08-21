1455516e7067cb5f622ca5dddea8653614f6ec3f
astrulmr <52928390+astrulmr@users.noreply.github.com>
2019-08-21 16:29:53 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -51,7 +51,7 @@ client.on('ready' , () => {
   
 
  client.on('message', message => {
-   if (message.channel3.content === 'brawlball') {
+   if (channel3.message.content === 'brawlball') {
       
            const embed = new RichEmbed()
            
@@ -64,7 +64,7 @@ client.on('ready' , () => {
   }
 })
 client.on('message', message => {
- if (message.channel3.content === '!@') {
+ if (channel3.message.content === '!@') {
     
          const embed = new RichEmbed()
          
@@ -75,7 +75,7 @@ client.on('message', message => {
 }
 })
 client.on('message', message => {
-  if (message.channel3.content === '!#') {
+  if (channel3.message.content === '!#') {
      
           const embed = new RichEmbed()
           
