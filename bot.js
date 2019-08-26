c3b6e8fe0697f207cca300bdb98503b688fba8af
astrulmr <52928390+astrulmr@users.noreply.github.com>
2019-08-26 20:10:19 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -673,15 +673,7 @@ message.channel.send("power not set")
  
 ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
-client.on("message", async message => {
 
-  if(message.content === "ping") {
-    // Calculates ping between sending a message and editing it, giving a nice round-trip latency.
-    // The second ping is an average latency between the bot and the websocket server (one-way, not round-trip)
-    const m = await message.channel.send("Ping?");
-    m.edit(`Pong! Latency is ${m.createdTimestamp - message.createdTimestamp}ms. API Latency is ${Math.round(client.ping)}ms`);
-  }
-})
 ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 client.on('guildMemberAdd', member => {
   const channel2 = member.guild.channels.get("535540529379672074")
