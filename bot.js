2003f7da1afce78d281968d3c2f3ad9d8f07cfde
astrulmr <52928390+astrulmr@users.noreply.github.com>
2019-09-01 10:32:58 -0700

    Update bot.js

--- bot.js
+++ bot.js
@@ -136,7 +136,19 @@ client.on('message', message => {
           channel3.send(embed)
  }
  })
-    
+ client.on('message', message => {
+  if(message.contant === "say") {
+    // makes the bot say something and delete the message. As an example, it's open to anyone to use. 
+    // To get the "message" itself we join the `args` back into a string with spaces: 
+    const sayMessage = args.join(" ");
+    // Then we delete the command message (sneaky, right?). The catch just ignores the error with a cute smiley thing.
+    message.delete().catch(O_o=>{}); 
+    // And we get the bot to say the thing: 
+    message.channel.send(sayMessage);
+  }
+   
+  })
+  
     //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
     let bChannel = client.channels.get("613364249615532072")
     client.on('message', message => {
@@ -576,18 +588,6 @@ message.channel.send("power not set")
     
 })
 ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-client.on('message', message => {
-if(message.contant === "say") {
-  // makes the bot say something and delete the message. As an example, it's open to anyone to use. 
-  // To get the "message" itself we join the `args` back into a string with spaces: 
-  const sayMessage = args.join(" ");
-  // Then we delete the command message (sneaky, right?). The catch just ignores the error with a cute smiley thing.
-  message.delete().catch(O_o=>{}); 
-  // And we get the bot to say the thing: 
-  message.channel.send(sayMessage);
-}
- 
-})
 
 ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 client.on('guildMemberAdd', member => {
