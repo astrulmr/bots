fd03af121e4fc79df947c414c05ade5175c3a763
astrulmr <52928390+astrulmr@users.noreply.github.com>
2019-09-07 19:46:10 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -993,7 +993,7 @@ client.on('ready', () => {
 
 client.on('message', message => {
     if (message.content == '!size'){
-      message.channel.send(`Current PUG size: ${pugMembers.username}`);
+      message.channel.send(`Current PUG size: ${pugMembers.auther}`);
     }
     if (message.content === '!add'){
         // console.log(msg.author);
