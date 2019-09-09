fa2c61ba3a4d6d47305ad315cde3d76755f6fdba
astrulmr <52928390+astrulmr@users.noreply.github.com>
2019-09-09 19:33:47 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -6,19 +6,6 @@ client.on('ready' , () => {
     console.log("connected as " + client.user.tag)
 
 
-    client.on('message', message => {
-   
-const args = message.content.slice(prefix.length).split(' ');
-const command = args.shift().toLowerCase();
-
-    if (command === 'args-info') {
-      if (!args.length) {
-        return message.channel.send(`You didn't provide any arguments, ${message.author}!`);
-      }
-    
-      message.channel.send(`Command name: ${command}\nArguments: ${args}`);
-    }})
-
 
 
     client.user.setActivity("BrawlBall")
