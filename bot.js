044bdcd18de2587ff85ab5d9ecb9747767349155
astrulmr <52928390+astrulmr@users.noreply.github.com>
2019-09-09 19:32:31 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -7,8 +7,7 @@ client.on('ready' , () => {
 
 
     client.on('message', message => {
-      if (!message.content.startsWith(prefix) || message.author.bot) return;
-
+   
 const args = message.content.slice(prefix.length).split(' ');
 const command = args.shift().toLowerCase();
 
