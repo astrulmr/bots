396052c7bf44ad40a98778e407f05d59689bc4e8
astrulmr <52928390+astrulmr@users.noreply.github.com>
2019-09-10 18:57:53 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -3545,6 +3545,16 @@ client.on('guildMemberAdd', member => {
           }
          
         })
-
-
+const prefix='!';
+        const args = message.content.slice(prefix.length).trim().split(/ +/g);
+        const command = args.shift().toLowerCase();
+        if(command === "say") {
+          // makes the bot say something and delete the message. As an example, it's open to anyone to use. 
+          // To get the "message" itself we join the `args` back into a string with spaces: 
+          const sayMessage = args.join(" ");
+          // Then we delete the command message (sneaky, right?). The catch just ignores the error with a cute smiley thing.
+          message.delete().catch(O_o=>{}); 
+          // And we get the bot to say the thing: 
+          message.channel.send(sayMessage);
+        }
 client.login(process.env.BOT_TOKEN)
