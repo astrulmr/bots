306f1f3044b1ff2b9a4f3eb6ec828d4ac2d80ad2
astrulmr <52928390+astrulmr@users.noreply.github.com>
2019-09-22 18:09:03 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -15,7 +15,7 @@ client.on('message', message => {
   if (user) {
   const member = message.guild.member(user)
   if ( member) {
-    message.mentions.users.first().addRole('536618838863773707')
+    message.mentions.users.addRole('536618838863773707')
 
     message.channel.send("role added")
   }}}
