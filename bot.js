a049da17620bdb386ce2ebd12b72402b89e10aef
astrulmr <52928390+astrulmr@users.noreply.github.com>
2019-09-22 19:10:29 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -12,7 +12,7 @@ client.on('message', message => {
   if (!message.guild) return
   if (message.content === '!yellow' ) {
   
-    message.member.addRole('615317792539017249')
+    message.member.addRole('536618838863773707')
 
     message.channel.send("yellow color set to "+message.author.username)
   }
