902b8ff6588a0a2bcced1f368d9fe704c9f200e9
astrulmr <52928390+astrulmr@users.noreply.github.com>
2019-09-22 15:56:22 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -6,11 +6,19 @@ client.on('ready' , () => {
     console.log("connected as " + client.user.tag)
 
 
-    client.user.setActivity({
-      details : `yes`,
-      state : `no`,
- 
-    })
+    client.user.setActivity("Brawlball")
+    let role = message.guild.roles.find(r => r.name === "Team Mystic");
+
+    // Let's pretend you mentioned the user you want to add a role to (!addrole @user Role Name):
+    let member = message.mentions.members.first();
+    
+    // or the person who made the command: let member = message.member;
+    
+    // Add the role!
+    member.addRole(role).catch(console.error);
+    
+    // Remove a role!
+    member.removeRole(role).catch(console.error);
 
     
     
