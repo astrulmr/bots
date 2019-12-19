a572d80533d4123030c2e39287661e457103abed
astrulmr <52928390+astrulmr@users.noreply.github.com>
2019-12-19 17:39:50 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,6 +1,5 @@
 const  Discord = require('discord.js');
 const { Client, RichEmbed,Attachment } = require('discord.js');
-const client = new Discord.Client()
 
 client.on('ready' , () => {
     console.log("connected as " + client.user.tag)
