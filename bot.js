5b05cf7694938bf2217afb1ef3602f94e5b89e28
astrulmr <52928390+astrulmr@users.noreply.github.com>
2019-12-28 21:10:06 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -935,12 +935,12 @@ client.on('message', message => {
   if (user) {
   const member = message.guild.member(user)
   if ( member) {
-    message.channel.send("! <@"+user.id+">")
+    message.channel.send("<@"+user.id+">")
  }}}
 })
 
 client.on('message', message => {
-  if (message.contect === "! <@482162074097549333>") {
+  if (message.contect === "<@482162074097549333>") {
     let embed = new Discord.RichEmbed()
     .setTitle('------------ASTRU------------')
   .setImage('https://www.brawlhalla.com/c/uploads/2018/11/Artemis.png')
