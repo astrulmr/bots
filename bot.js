43be5293ce5841c48cc96af1979c9ad58f221197
astrulmr <52928390+astrulmr@users.noreply.github.com>
2020-03-05 01:24:06 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -5,7 +5,7 @@ const client = new Discord.Client()
 client.on('ready' , () => {
     console.log("connected as " + client.user.tag)
 
-    client.user.setActivity("kvvkk")
+    client.user.setActivity("kv33vkk")
 
 
 client.on('message', message => {
