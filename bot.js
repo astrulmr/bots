89f0bdac001123477a1c0dfa98291bc0503096ae
astrulmr <52928390+astrulmr@users.noreply.github.com>
2020-04-30 01:58:40 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1033,6 +1033,7 @@ bChannel.send('OFC im the best ez pz');
 if(message.deletable) message.delete()
 if(args.length < 1)
 return message.reply("Nothing to say?").then(m => m.delete(5000))
+const roleColor = message.guild.me.displayHexColor ==="#000000" ? "#ffffff" : message.guild.me.displayHexColor
 message.channel.send(args.join(" "))
 
     }
