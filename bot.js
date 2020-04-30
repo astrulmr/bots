13933e415ff47c9c6d31f6217e7d96008d9164d8
astrulmr <52928390+astrulmr@users.noreply.github.com>
2020-04-30 08:59:02 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1025,9 +1025,27 @@ bChannel.send('OFC im the best ez pz');
 
     }})
 
-    if (member.react === ':thumbsup:') {
-        chanel.send("hi")
-    }
+    message.react('👍').then(() => message.react('👎'));
+
+    const filter = (reaction, user) => {
+        return ['👍', '👎'].includes(reaction.emoji.name) && user.id === message.author.id;
+    };
+    
+    message.awaitReactions(filter, { max: 1, time: 60000, errors: ['time'] })
+        .then(collected => {
+            const reaction = collected.first();
+    
+            if (reaction.emoji.name === '👍') {
+                message.reply('you reacted with a thumbs up.');
+            }
+            else {
+                message.reply('you reacted with a thumbs down.');
+            }
+        })
+        .catch(collected => {
+            console.log(`After a minute, only ${collected.size} out of 4 reacted.`);
+            message.reply('you didn\'t react with neither a thumbs up, nor a thumbs down.');
+        });
  
 
     client.on('message', message => {
