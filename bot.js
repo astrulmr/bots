a00c6b96428c6088432da00ef28f2c0c2325a36a
astrulmr <52928390+astrulmr@users.noreply.github.com>
2020-04-30 04:13:49 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1062,8 +1062,7 @@ bChannel.send('OFC im the best ez pz');
             if (args[0].toLowerCase() === "embed") {
                 const embed = new RichEmbed()
                     .setDescription(args.slice(1).join(" "))
-                    .setColor(roleColor === "#000000" ? "#ffffff" :  roleColorv)
-                    .setTimestamp()
+                    .setColor("#000000")
                     .setImage(client.user.displayAvatarURL)
                     .setAuthor(message.author.username, message.author.displayAvatarURL);
     
