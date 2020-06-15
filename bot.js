60d31d5c739aa71d236b6ebcdb8288f9cc55c800
astrulmr <52928390+astrulmr@users.noreply.github.com>
2020-06-15 11:15:07 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -194,7 +194,7 @@ client.on('ready', () => {
 
     client.on('message', message => {
         if (message.content === '!hi') {
-            message.reply(`Hello  What Can i Do For You ? \n 1- !who@anyname \n 2- !avatar @anyname \n 3- !os clan \n 4- !help `);
+            message.reply(`Hello  What Can i Do For You ? \n 1- !who @anyname \n 2- !avatar @anyname \n 3- !os clan \n 4- !help `);
         }
     })
     let channel3 = client.channels.get("613662955522752516")
