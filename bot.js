4642871e078fbf8f4e9fd05979cfd691c9993145
astrulmr <52928390+astrulmr@users.noreply.github.com>
2020-06-17 02:00:11 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -2382,6 +2382,57 @@ if (message.content === "!ap" ) {
 ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+client.on("message", async message => {
+  const prefix = "#";
+
+  // If the author's a bot, return
+  // If the message was not sent in a server, return
+  // If the message doesn't start with the prefix, return
+  if (message.author.bot) return;
+  if (!message.guild) return;
+  if (!message.content.startsWith(prefix)) return;
+
+  // Arguments and command variable
+  // cmd is the first word in the message, aka the command
+  // args is an array of words after the command
+  // !say hello I am a bot
+  // cmd == say (because the prefix is sliced off)
+  // args == ["hello", "I", "am", "a", "bot"]
+  const args = message.content.slice(prefix.length).trim().split(/ +/g);
+  const cmd = args.shift().toLowerCase();
+
+  if (cmd === "ping") {
+      // Send a message
+      const msg = await message.channel.send(`🏓 Pinging....`);
+
+      // Edit the message
+      msg.edit(`🏓 Pong!\nLatency is ${Math.floor(msg.createdTimestap - message.createdTimestap)}ms\nAPI Latency is ${Math.round(client.ping)}ms`);
+  }
+
+  if (cmd === "say") {
+      // Check if you can delete the message
+      if (message.deletable) message.delete();
+
+      if (args.length < 0) return message.reply(`Nothing to say?`).then(m => m.delete(5000));
+
+      // Role color
+      const roleColor = message.guild.me.highestRole.hexColor;
+
+      // If the first argument is embed, send an embed,
+      // otherwise, send a normal message
+      if (args[0].toLowerCase() === "embed") {
+          const embed = new RichEmbed()
+              .setDescription(args.slice(1).join(" "))
+              .setColor("#000000")
+
+          message.channel.send(embed);
+      } else {
+          message.channel.send(args.join(" "));
+      }
+  }
+});
+
+
 ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
