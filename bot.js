f12ba48214474248fc888cea1cae3e32270daa77
astrulmr <52928390+astrulmr@users.noreply.github.com>
2020-06-16 22:59:51 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -2453,47 +2453,53 @@ client.on('message', message => {
 ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-client.on('message', message => {
-const bbroomenter = member.guild.channels.get("594177565825171457")
-const bbroomjoin = member.guild.channels.get("713502234335510539")
 
-const prefix1 = "#";
-if (!bbroomenter) return
-const args = message.content.slice(prefix1.length).trim().split(/ +/g);
-const cmd1 = args.shift().toLowerCase();
-if (cmd1 === "say") {
-var msg1 = Array(5);
-msg1[1] = `brawlball time start now , Room #`+args.slice(1).join(" ");
-msg1[2] = `brawlball time start now , Room #`+args.slice(1).join(" ");
-msg1[3] = `brawlball time start now , Room #`+args.slice(1).join(" ");
-msg1[4] = `brawlball time start now , Room #`+args.slice(1).join(" ");
-msg1[5] = `brawlball time start now , Room #`+args.slice(1).join(" ");
-msg1[6] = `brawlball time start now , Room #`+args.slice(1).join(" ");
-var x = getRandomInt(0, 20);
-if (x < 5) {
-    if (x < 3) {
-      bbroomjoin.send(msg1[1]);
-    }
-    else {
-      bbroomjoin.send(msg1[3]);
-    }
-}
-else if (x <= 9) {
-    if (x >= 7) {
-      bbroomjoin.send(msg1[2]);
-    }
-    else {
-      bbroomjoin.send(msg1[4]);
-    }
-}
-else if (x <= 12) {
-  bbroomjoin.send(msg1[5]);
-}
-else {
-  bbroomjoin.send(msg1[6])
-}}
 
-})
+
+
+
+client.on("message", async message => {
+  const prefix1 = "!";
+
+  // If the author's a bot, return
+  // If the message was not sent in a server, return
+  // If the message doesn't start with the prefix, return
+  if (message.author.bot) return;
+  if (!message.guild) return;
+  if (!message.content.startsWith(prefix1)) return;
+
+  // Arguments and command variable
+  // cmd is the first word in the message, aka the command
+  // args is an array of words after the command
+  // !say hello I am a bot
+  // cmd == say (because the prefix is sliced off)
+  // args == ["hello", "I", "am", "a", "bot"]
+  const args = message.content.slice(prefix1.length).trim().split(/ +/g);
+  const cmd1 = args.shift().toLowerCase();
+
+
+  if (cmd1 === "#") {
+      // Check if you can delete the message
+      if (message.deletable) message.delete();
+
+      if (args.length < 0) return message.reply(`Nothing to say?`).then(m => m.delete(5000));
+
+      // Role color
+      const roleColor = message.guild.me.highestRole.hexColor;
+
+      // If the first argument is embed, send an embed,
+      // otherwise, send a normal message
+      if (args[0].toLowerCase() === "embed") {
+          const embed = new RichEmbed()
+              .setDescription(args.slice(1).join(" "))
+              .setColor("#000000")
+
+          message.channel.send(embed);
+      } else {
+          message.channel.send(args.join(" "));
+      }
+  }
+});
 
 ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
