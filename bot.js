8bddfd8695fe566d7139b65f6f6e447eb0f3e6ab
astrulmr <52928390+astrulmr@users.noreply.github.com>
2020-06-23 22:11:15 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -2440,7 +2440,6 @@ client.on("message", async message => {
             
              if(!message.member.hasPermission(["MANAGE_MESSAGES"])) return message.channel.send("You do not have permission to  this")
 else
-        
           bbchannel.send(embed);
 
       
