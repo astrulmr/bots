6d776ce7b854ba47f603bbf42f389ea66d456e97
astrulmr <52928390+astrulmr@users.noreply.github.com>
2020-06-30 12:03:21 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -6,7 +6,7 @@
 const  Discord = require('discord.js');
 const { Client, RichEmbed,Attachment } = require('discord.js');
 const client = new Discord.Client()
-const cron = require('node-cron');
+const cron = require('cron');
 
 
 client.on('ready' , () => {
