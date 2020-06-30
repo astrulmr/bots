5dbb7b6b3689284a10e231331a18ee7599205b72
astrulmr <52928390+astrulmr@users.noreply.github.com>
2020-06-30 08:08:56 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -210,10 +210,7 @@ birthdaychannel.send("birthday test")
  })
  let channel3 = client.channels.get("613662955522752516")
  
- client.on('message', message => {
-  if (message.content === 'u so nub hoho')
-   birthdaychannel.send("u so nub hoho")
- })
+
  
 
 
