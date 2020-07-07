8f810bf335cf2a9cb687787227b7932464d0783c
astrulmr <52928390+astrulmr@users.noreply.github.com>
2020-07-07 21:41:19 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -11,8 +11,9 @@ client.on('ready' , () => {
     console.log("connected as " + client.user.tag)
 
 let sttius =[
-  client.user.setActivity("Brawlhalla",{type: "PLAYING"}),
-  client.user.setActivity("Brawlball",{type: "PLAYING"})
+  
+  client.user.setActivity("Brawlball",{type: "PLAYING"}),
+  client.user.setActivity("www.youtube.com/watch?v=KV-8PCeqPzg&t=42s",{type: "WATCHING"})
 ]
 let sttius2 =[
   "www.youtube.com/watch?v=KV-8PCeqPzg&t=42s",
