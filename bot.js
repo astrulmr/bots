8428b03b85e878ff11ef256a13f0a79e7c6c6d73
astrulmr <52928390+astrulmr@users.noreply.github.com>
2020-07-25 16:12:07 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -2129,12 +2129,7 @@ recchannel.send("!strt")
 
             }).catch(function() {
              
-             })})})
-
-
-             client.on('ready' , () => {
-              let recchannel = client.channels.get("736380370651709531")
-
+             })})
              
               let embed = new Discord.RichEmbed()
               .setTitle('These are the positions in Brawlball')
