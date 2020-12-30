a64b6a3687efad2eb57e921d12428b76f6b51812
astrulmr <52928390+astrulmr@users.noreply.github.com>
2020-12-31 02:17:15 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -2540,7 +2540,7 @@ client.on('message', message => {
  if(message.content == `!runners`) {
         const ListEmbed = new Discord.RichEmbed()
             .setTitle('Runners in brawlball:')
-            .setDescription(message.guild.roles.get('736222790516211723').members.map(m=>m.user).join('\n\n'));
+            .setDescription("**"+message.guild.roles.get('736222790516211723').members.map(m=>m.user.username).join('\n')+"**");
         message.channel.send(ListEmbed);                    
     }
 });
