9cc805b73db29d9134746511572a807e08a8b0b5
astrulmr <52928390+astrulmr@users.noreply.github.com>
2020-12-31 12:56:25 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -2543,9 +2543,9 @@ client.on('message', message => {
             .setColor('#ff0000')
             .setThumbnail("https://www.esite.co.ke/static/assets/img/run.png")
             .setDescription("Here are all players positions")
-            .addField("Runners",message.guild.roles.get('730019439180775434').members.map(m=>m.user.username).join('**\n :traffic_light: :traffic_light: :traffic_light: \n**'),true)
-            .addField("Supporters",message.guild.roles.get('730019439180775434').members.map(m=>m.user.username).join('\n :traffic_light: :traffic_light: :traffic_light: \n'),true)
-            .addField("defeders",message.guild.roles.get('730019439180775434').members.map(m=>m.user.username).join('\n :traffic_light: :traffic_light: :traffic_light: \n'),true)
+            .addField("Runners",message.guild.roles.get('736222790516211723').members.map(m=>m.user.username).join('**\n :traffic_light: :traffic_light: :traffic_light: \n**'),true)
+            .addField("Supporters",message.guild.roles.get('736222933827190835').members.map(m=>m.user.username).join('\n :traffic_light: :traffic_light: :traffic_light: \n'),true)
+            .addField("defeders",message.guild.roles.get('736222973140402246').members.map(m=>m.user.username).join('\n :traffic_light: :traffic_light: :traffic_light: \n'),true)
             message.channel.send(ListEmbed);                    
     }
 });
