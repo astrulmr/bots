eca87dc102ceec4aecc6ec00d42ba0776c380abf
astrulmr <52928390+astrulmr@users.noreply.github.com>
2021-01-03 12:07:30 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -2606,12 +2606,10 @@ var lojok=new Array();
 while(args.length){
   const ndx = Math.random() * args.length | 0;
     const elem = args.splice(ndx,1)[0];
-  /*  if (i % 3 == 0)
+    if (jargs.length % 3 == 0)
   {
       lojok.push("this team is")
-      
-  
-  }*/
+  }
   //message.channel.send(i);
   lojok.push(elem)
   
