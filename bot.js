5438ffd95febd0ab52fb1b372dbaa79cbb890966
astrulmr <52928390+astrulmr@users.noreply.github.com>
2021-01-03 13:16:51 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -2629,7 +2629,7 @@ client.on('messageReactionAdd', (reaction, user) => {
     message.guild.fetchMember(user.id).then(member => {
       if(user.bot)  return;
     else { 
-         message.channel.send("ez"+reaction.users.map(u => u.toString()))
+         message.channel.send("ez"+reaction.users.map(u => u.user.username))
          console.log(reaction.users.username);}
     })
     }}});
